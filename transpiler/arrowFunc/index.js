df9ed9c0b3fd97786cd50061f42784b4dd7e779e
Felix Tripier <ftripier@gmail.com>
2017-07-23 15:30:04 -0700

    Transform finished

--- transpiler/arrowFunc/index.js
+++ transpiler/arrowFunc/index.js
@@ -3,6 +3,14 @@ const t = require('babel-types');
 
 module.exports = code => {
   return transform(code, {
-    ArrowFunctionExpression(node) {},
+    ArrowFunctionExpression(node) {
+      let newBody = node.body;
+      if (newBody && newBody.type !== "BlockStatement") {
+        newBody = t.blockStatement(
+          [t.returnStatement(newBody)]
+        );
+      }
+      return t.functionExpression(node.id, node.params, newBody);
+    },
   });
 };
--- transpiler/transform/index.js
+++ transpiler/transform/index.js
@@ -2,6 +2,34 @@ const babylon = require('babylon');
 const t = require('babel-types');
 const generate = require('babel-generator').default;
 
-function transform(code, visitors) {}
+function walk(node, visitors) {
+  if (!isNode(node)) return node;
 
-mmodule.exports = transform;
+  let modified = visitors[node.type] ? visitors[node.type](node) : node;
+
+  for (const key in modified) {
+    if (Array.isArray(modified[key])) {
+      modified[key] = modified[key].map((child) => {
+        return walk(child, visitors);
+      });
+    } else if (isNode(modified[key])) {
+      modified[key] = walk(modified[key], visitors);
+    }
+  }
+
+  return modified;
+}
+
+function transform(code, visitors) {
+  const ast = babylon.parse(code);
+
+  walk(ast.program, visitors);
+
+  return generate(ast).code;
+}
+
+function isNode(node) {
+  return node && node.type != null;
+}
+
+module.exports = transform;
