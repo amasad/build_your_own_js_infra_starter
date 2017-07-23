2b2b5ce1671ec2610ddd51184dd2b707b3d1551d
Amjad Masad <amjad.masad@gmail.com>
2017-07-23 14:32:01 -0700

    draft

--- transpiler/transform/index.js
+++ transpiler/transform/index.js
@@ -2,6 +2,21 @@ const babylon = require('babylon');
 const t = require('babel-types');
 const generate = require('babel-generator').default;
 
-function transform(code, visitors) {}
+function transform(code, visitors) {
+  const ast = babylon.parse(code);
+
+  // visitors: {
+  //   NumericLiteral: function() {}
+  // }
+
+  // go through ast and
+  // for every node
+  // check its type matches a visitor
+  // call the handler for the visitor
+}
+
+function isNode(node) {
+  return node && typeof node === 'object' && t.TYPES.includes(node.type);
+}
 
 mmodule.exports = transform;
