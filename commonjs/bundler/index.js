dcfd19016576271f19b31c38d1c72aff8c22c205
Felix Tripier <ftripier@gmail.com>
2017-07-23 13:40:31 -0700

    finished commonjs

--- commonjs/bundler/index.js
+++ commonjs/bundler/index.js
@@ -3,9 +3,34 @@ const fs = require('fs');
 
 const requireRe = /\brequire\s*?\(\s*?(['"])([^"']+)\1\s*?\)/g;
 
-function bundle(entry) {
-  // resolve
-  // bundle
-  // return source string
+function wrap(source) {
+  return [
+    '(function(module) {',
+    'module.exports = {};',
+    'let exports = module.exports;',
+    source,
+    'return module.exports;',
+    '})({})'
+  ].join('\n');
 }
+
+function bundle(entry) {
+  const source = fs.readFileSync(entry);
+
+  if (!source.match(requireRe)) {
+    return source;
+  }
+
+  const replaces = source.replace(requireRe, (match) => {
+    const matchPath = match.replace('require(', '').replace(')', '').replace(/"/g, '');
+    const resolvedMatch = resolver(entry, matchPath);
+    const inlinedSource = bundle(resolvedMatch);
+    const wrappedSource = wrap(inlinedSource);
+
+    return wrappedSource;
+  });
+
+  return replaces;
+}
+
 module.exports = { bundle };
